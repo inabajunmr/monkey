0f9692c1bbfacb0dbbc181fd1f0e3ef573e94dbc
inabajunmr <inabajun.for.regi@gmail.com>
2020-03-24 00:06:57 +0900

    44

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/inabajunmr/monkey/ast"
 	"github.com/inabajunmr/monkey/lexer"
 	"github.com/inabajunmr/monkey/token"
@@ -11,14 +12,25 @@ type Parser struct {
 
 	curToken token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l:l}
+	p := &Parser{l:l , errors:[]string{}}
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func(p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -83,6 +95,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
