d2286199427473947fc151ac0e9ece5f2c04fb39
inabajunmr <inabajun.for.regi@gmail.com>
2020-04-19 01:58:33 +0900

    134

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/inabajunmr/monkey/evaluator"
 	"github.com/inabajunmr/monkey/lexer"
 	"github.com/inabajunmr/monkey/parser"
 	"io"
@@ -64,10 +65,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
-
 }
 
 func printParserErrors(out io.Writer, errors []string) {
