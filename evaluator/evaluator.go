c9ac29a3f87fd2a11ec1e91fa7e98191c89be37d
inabajunmr <inabajun.for.regi@gmail.com>
2020-04-25 13:18:50 +0900

    225

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -91,6 +91,30 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return evalIndexExpression(left, index)
+	case *ast.HashLiteral:
+		pairs := make(map[object.HashKey]object.HashPair)
+
+		for keyNode, valueNode := range node.Pairs {
+			key := Eval(keyNode, env)
+			if isError(key) {
+				return key
+			}
+
+			hashKey, ok := key.(object.Hashable)
+			if !ok {
+				return newError("unusable as hash key: %s", key.Type())
+			}
+
+			value := Eval(valueNode, env)
+			if isError(value) {
+				return value
+			}
+
+			hashed := hashKey.HashKey()
+			pairs[hashed] = object.HashPair{Key: key, Value: value}
+		}
+
+		return &object.Hash{Pairs: pairs}
 	}
 
 	return nil
