579c9d65d258d4de8504cceed0af816724a3b0d3
inabajunmr <inabajun.for.regi@gmail.com>
2020-04-25 13:43:15 +0900

    231

--- evaluator/builtin.go
+++ evaluator/builtin.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "github.com/inabajunmr/monkey/object"
+import (
+	"fmt"
+	"github.com/inabajunmr/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -89,6 +92,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
