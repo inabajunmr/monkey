42bfd5eaa3c766440b7df78595221fb775031153
inabajunmr <inabajun.for.regi@gmail.com>
2020-04-25 13:40:48 +0900

    23-0

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -207,6 +207,7 @@ if (10 > 1) {
 			"unknown operator: BOOLEAN + BOOLEAN"},
 		{"foobar", "identifier not found: foobar"},
 		{`"Hello" - "World"`, "unknown operator: STRING - STRING"},
+		{`{"name": "Monkey"}[fn(x){ x }];"`, "unusable as hash key: FUNCTION"},
 	}
 
 	for _, tt := range tests {
@@ -440,5 +441,31 @@ let two = "two";
 		}
 
 		testIntegerObject(t, pair.Value, expectedValue)
+	}
+}
+
+func TestHashIndexExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`{"foo": 5}["foo"]`, 5},
+		{`{"foo": 5}["bar"]`, nil},
+		{`let key = "foo"; {"foo": 5}[key]`, 5},
+		{`{}["foo"]`, nil},
+		{`{5: 5}[5]`, 5},
+		{`{true: 5}[true]`, 5},
+		{`{false: 5}[false]`, 5},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+
 	}
 }
